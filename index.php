152fb5ddd059fd98e0126ecae6d9343f12c6f9c2
Josh Braendel <Jbud@live.ca>
2025-06-01 11:53:38 -0700

    missing changes

--- index.php
+++ index.php
@@ -3,30 +3,24 @@
  TODO
   - localStorage of crossword + timestamp created + timestamp touched;  request puzzle from server if local.created < server.created (puz updated); request user save state (and then replace w/ local local storage if needed)
   - automate processing and uploading puzzles 
+  - mysql key for users: client, downloadpuz
+  - crossword json version: migrate up to convert the json from earlier version to later version: eg. change  `clues: { original: "nyt", revised: "chatgpt" }` to `clues: { original: "nyt", revised: "chatgpt", revisedEasyVersion: "" }
+  - puzzle list: cache in localStorage -> fetch ajax -> compare against localStorage -> if changed then update localStorage and reload(?); only fetch puzzles within a specified range on ajax
   - puzzle sql: store originalData + processedData separately; also minimize processedData size (eg. state unnecessary)
   - php config to disable cache or turn back on (for when we're in development or not)
-  - cache puzzlesList sql results? (since it rarely changes)
-  - compress php data sending down (since its massive but all json text)
-  - button to reveal random cells
   - fit board height to mobile screen, including (smaller) clue display -- ie. larger boards overflow
   - clue text could be too large, need to scroll down to see the rest? maybe remove fixed position and instead have it directly under the board
   - animation when ending?
   - calendar page to show which days you completed or are partial completed  (can extend this into daily trivia/etc.)
-  - press/hold to unveil non-ai clue (let go brings it back to AI)
 
 
   - dreamhost security errors + .git folder
 
-  - store openai key and nyt key in php config; pass along -- shouldn't matter if we perform offline
-  - check pricing for prompts (how much per crossword?) -- can we merge requests to spend less tokens? or have saved prompts?
-  - fix dreamhost nodejs specification (wrong nodejs) -- shouldn't matter if we don't use ai in client
+  - check pricing for prompts (how much per crossword?) -- can we merge requests to spend less tokens? or have saved prompts? what about Gemini?
 
   - virtual keyboard lib: https://furcan.github.io/KioskBoard/
   - support for more than NYT
 -->
-<?php
-include('controller.php');
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -41,17 +35,18 @@ include('controller.php');
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+<!--
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&amp;display=swap" rel="stylesheet">
+-->
     <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&amp;display=swap" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js" integrity="sha512-qtX0GLM3qX8rxJN1gyDfcnMFFrKvixfoEOwbBib9VafR5vbChV5LeE5wSI/x+IlCkTY5ZFddFDCCfaVJJNnuKQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
-<script>
-var allPuzzles = <?php echo getAllPuzzles(); ?>;
-</script>
     <input type="password" id="hiddenInput" maxlength="1" style="opacity: 0; position: fixed; bottom: 0px; left: 0px;" />
     
     <div id="puzzleInfo">
@@ -90,7 +85,7 @@ var allPuzzles = <?php echo getAllPuzzles(); ?>;
         </div>
     </div>
     <script src="crossword.js?nocache=<?php echo time(); ?>"></script>
-    <script src="main.js?nocache=<?php echo time(); ?>"></script>
+    <script src="main.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
     <script>eruda.init();</script>
